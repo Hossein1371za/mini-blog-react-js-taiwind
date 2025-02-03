9c5604ec2f007737ce307e47836bd1532d89526e
Hossein1371za <157877762+Hossein1371za@users.noreply.github.com>
2025-02-03 22:32:28 +0330

    show comment

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import swal from "sweetalert2";
+import { useParams } from "react-router-dom";
 
 const Comments = () => {
-  return (
-    <div>Comments</div>
-  )
+  const [description, setDescription] = useState("");
+  const [comment,setComment] = useState([])
+  const { id } = useParams();
+  let user_id = JSON.parse(localStorage.getItem("user_id"));
+
+
+  useEffect(() => {
+    getComment();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+        commentBody: description,
+        postId: id, 
+        userId: user_id, 
+    };
+    try {
+        const res = await axios.post("/comments", data);
+        if (res.data.error) {
+            swal.fire({
+                icon: "error",
+                title: "خطا!",
+                text: res.data.error,
+                showConfirmButton: true,
+                confirmButtonText: "تایید!",
+            });
+        } else {
+            swal.fire({
+                icon: "success",
+                title: "تبریک میگم!",
+                text: res.data.message,
+                showConfirmButton: true,
+                confirmButtonText: "تایید!",
+            });
+            setDescription(""); 
+            getComment()
+        }
+    } catch (error) {
+
+        swal.fire({
+            icon: "error",
+            title: "خطا!",
+            text: "مشکلی پیش آمد.",
+            showConfirmButton: true,
+            confirmButtonText: "تایید!",
+        });
+    }
+};
+const getComment = async ()=>{
+  await axios.get(`/comments/${id}`).then((res)=>{
+    setComment(res.data)
+  })
 }
 
-export default Comments
+  return (
+    <div className="flex flex-col items-center gap-y-5">
+      <h2 className="font-bold text-2xl pt-3">نظرات شما</h2>
+      <form
+        className="w-full flex flex-col items-center gap-y-5"
+        onSubmit={handleSubmit}
+      >
+        <textarea
+          className="outline-none border-2 border-slate-300 w-full p-4 rounded-lg text-lg"
+          placeholder="نظرتان را با من به اشتراک بگذارید..."
+          onChange={(e) => setDescription(e.target.value)}
+          name="description"
+          value={description}
+        ></textarea>
+        <button className="text-lg bg-secondary py-2 w-[50%] rounded-md hover:bg-green-400 cursor-pointer transition-all duration-300">
+          ارسال نظر
+        </button>
+      </form>
+      <div className="flex flex-col items-center gap-4 w-full h-[250px] overflow-y-scroll">
+        {
+          comment && comment.map((item)=>(
+           <div className="bg-slate-300 p-3 w-full rounded-md" key={item.id}>
+             <small className="font-bold">{item.user.name}:</small>
+            <h3>
+              {item.commentBody}
+            </h3>
+           </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
--- src/components/Detailes.jsx
+++ src/components/Detailes.jsx
@@ -4,19 +4,18 @@ import Comments from "./Comments";
 
 const Detailes = () => {
   const { state } = useLocation();
-console.log(state);
   return (
-    <div className="flex items-center justify-center h-screen container">
-      <div className="border-secondary  rounded-lg border-2">
+    <div className="flex lg:items-center justify-center lg:h-screen container mt-8 lg:mt-0 ">
+      <div className="border-slate-300 shadow-lg  rounded-lg border-2">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 p-5">
           <div className="border-2 border-slate-200 rounded-md p-3 shadow-md">
             <img className="h-[300px] lg:h-[350px]" src={state.url} alt="."/>
             <div className=" bg-slate-300 mt-4 p-3 rounded-md">
             <div className="flex items-center justify-between">
-              <h6>نویسنده: {state && state.user.name}</h6>
-              <h3>{state.title}</h3>
+              <h3 className="font-bold text-lg">نویسنده: {state && state.user.name}</h3>
+              <h6 className="font-bold">{state.title}</h6>
             </div>
-            <p className="text-center pt-3">{state.desc}</p>
+            <p className="text-center pt-3 text-slate-800">{state.desc}</p>
             </div>
           </div>
           <div className="border-2 border-slate-200 rounded-md p-3 shadow-md">
